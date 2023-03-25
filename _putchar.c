1d7e31833be3ae854115c06b9f333ddeab0d2db0
maqamylee0 <maqamylee0@gmail.com>
2023-03-25 13:29:07 -0700

    printf

---
+++ _putchar.c
@@ -0,0 +1,11 @@
+#include <unistd.h>
+
+/**
+ * main - main function
+ * Return: zero for success
+ */
+int _putchar(char c) 
+{
+    return write(1, "&c", 1);
+   
+}
---
+++ main.h
@@ -0,0 +1,7 @@
+#ifndef __MAIN_H_
+#define __MAIN_H_
+
+int _printf(const char *format, ...);
+int _putchar(char c);
+
+#endif
