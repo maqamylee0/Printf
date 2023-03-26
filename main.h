d8e4d8b2daa356fe06c422ec498fd1cc9e6f43dc
Peter Ochan <rempeterwisdom@gmail.com>
2023-03-26 01:21:17 -0700

    added prototypes & struct; removed _putchar prototype

--- main.h
+++ main.h
@@ -1,7 +1,30 @@
-#ifndef __MAIN_H_
-#define __MAIN_H_
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+#include <stdarg.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
 
 int _printf(const char *format, ...);
-int _putchar(char c);
+int (*check_specifier(const char *format))(va_list);
+int print_char(va_list args);
+int print_str(va_list args);
+int print_cent(va_list args);
+
+/**
+ * struct func - takes specifier
+ * and appropriate print
+ * @p: character to compare
+ * @f: function that prints
+ *
+ * Description: takes a specifier to be compared
+ * so appropriate printing action is taken
+ */
+typedef struct func
+{
+	char *p;
+	int (*f)(va_list);
+} func_t;
 
 #endif
