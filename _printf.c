14ca5e9cecbb7805eda6f642f8e560b6b4ddbd3b
Peter Ochan <rempeterwisdom@gmail.com>
2023-03-27 01:39:38 -0700

    minor change of va_start(args, format) in _printf.c

--- _printf.c
+++ _printf.c
@@ -20,6 +20,9 @@ int _printf(const char *format, ...)
 	if (!format || (format[0] == '%' && !format[1]))
 		return (-1);
 
+	if (format[0] == '%' && format[1] == ' ' && !format[2])
+		return (-1);
+
 	/* prints each character of string */
 	while (format[i])
 	{
