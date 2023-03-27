c0dffa4b543c303a142c4537502a364260b41f4c
Peter Ochan <rempeterwisdom@gmail.com>
2023-03-27 07:04:20 -0700

    reverted _printf to before 27

--- _printf.c
+++ _printf.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  * _printf - prints formated output
  * @format: pointer to string
@@ -8,23 +7,20 @@
  */
 int _printf(const char *format, ...)
 {
-	int i = 0, temp = 0, count = 0;
+	int i = 0;
+	int temp = 0;
+	int count = 0;
 	va_list args;
 	int (*f)(va_list);
-
+	va_start(args, format);
+	/* ensures NULL pointer isnt parsed */
 	if (format == NULL)
 		return (-1);
-
-	va_start(args, format);
-
+	
 	/* prints each character of string */
 	while (format[i])
 	{
-		if (format[i] == '%' && format[i + 1] == '\0')
-		{
-			return (-1);
-		}
-		if (format[i] != '%' && format[i + 1] != '\0')
+		if (format[i] != '%')
 		{
 			temp = write(1, &format[i], 1);
 			count += temp;
@@ -39,11 +35,16 @@ int _printf(const char *format, ...)
 				temp = f(args);
 				count = count + temp;
 				i = i + 2;
+				continue;
 			}
-			else
+			if (format[i + 1] == '\0')
+			{
+				break;
+			}
+			if (format[i + 1] != '\0')
 			{
 				temp = write(1, &format[i + 1], 1);
-				count = count + temp;
+				count += temp;
 				i = i + 2;
 				continue;
 			}
