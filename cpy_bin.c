e778bb29e8ae61859d1e99c3411a4a7665cb2b5b
maqamylee0 <maqamylee0@gmail.com>
2023-03-28 15:00:27 -0700

    handle b

--- cpy_bin.c
+++ cpy_bin.c
@@ -13,6 +13,8 @@ int cpy_bin(va_list args, char *buff, unsigned long int *buff_loc)
 	int count = 0;
 	int num = va_arg(args, int);
 	int rem;
+	int str[100];
+	int i = 0;
 	
 	if (num < 2)
 	{
@@ -26,15 +28,24 @@ int cpy_bin(va_list args, char *buff, unsigned long int *buff_loc)
 	{
 		rem = num % 2;
 		buff_check(buff, buff_loc);
-		buff[*buff_loc] = (char)(rem) + '0';
-		*buff_loc = *buff_loc + 1;
+		str[i++] = (char)(rem) + '0';
 		count++;
 		num /= 2;
+	
+	
 	}
+
 	buff_check(buff, buff_loc);
-	buff[*buff_loc] = (char)(num) + '0';
+	str[i] = (char)(num) + '0';
 	*buff_loc = *buff_loc + 1;
 	count++;
+	
+	while (i >= 0)
+	{
+		buff_check(buff, buff_loc);
+		buff[*buff_loc] = str[i--];
+		*buff_loc = *buff_loc + 1;
+	}
 
 	return (count);
 }
