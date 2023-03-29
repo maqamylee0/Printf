84027488fd5aeb4ce8543c4069b816b051ba33b3
Peter Ochan <rempeterwisdom@gmail.com>
2023-03-29 08:39:30 -0700

    modified mem allocation for char *str

--- cpy_bin.c
+++ cpy_bin.c
@@ -10,52 +10,40 @@
 int cpy_bin(va_list args, char *buff, unsigned long int *buff_loc)
 {
 
-	int count = 0;
-	int num = va_arg(args, int);
-	int rem;
+	int count, len = 0;
+	int num_1, num = va_arg(args, int);
 	char *str;
-	int i = 0;
+	int i;
 
+	num_1 = num;
 	if (num == 0)
 	{
-		buff_check(buff, buff_loc);
 		buff[*buff_loc] = '0';
 		*buff_loc = *buff_loc + 1;
+		buff_check(buff, buff_loc);
 		return (1);
 	}
-	str = malloc(sizeof(char) * num + 1);
+	for ( ; num_1 != 0; num_1 /= 2)
+		len++;
+
+	str = malloc(sizeof(char) * (len + 1));
 	if (!str)
-		return (-1)
+		return (-1);
 
+	count = len;
 
-	if (num < 2)
+	for (i = len - 1; i >= 0; i--)
 	{
-		buff_check(buff, buff_loc);
-		buff[*buff_loc] = (char)(num) + '0';
-		*buff_loc = *buff_loc + 1;
-		count++;
-		free(str);
-		return (count);
-	}
-	while (num > 0)
-	{
-		rem = num % 2;
-		buff_check(buff, buff_loc);
-		str[i++] = rem + '0';
-		count++;
-		num /= 2;
+		int rem = num % 2;
+
+		num = num / 2;
+		str[i] = rem + '0';
 	}
 
-	str[i] = '\0';
-	*buff_loc = *buff_loc + 1;
-	i--;
-
-	while (i >= 0)
+	for (i = 0; i < len; i++)
 	{
-		buff_check(buff, buff_loc);
-		buff[*buff_loc] = str[i--];
+		buff[*buff_loc] = str[i];
 		*buff_loc = *buff_loc + 1;
 	}
-
 	return (count);
 }
